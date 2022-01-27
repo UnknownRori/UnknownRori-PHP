40d0e2d761e76b56b3cc5d6802e08aa812322b79
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2022-01-27 17:16:35 +0700

    fixing automation bug

--- core/console/server.py
+++ core/console/server.py
@@ -1,15 +1,16 @@
 from os import system
-# php -S localhost:8080 ./public/index.php
+# php -S localhost:8080 -t ./public ./public/index.php
 
 class Server:
     def Start():
         # Customizable
         host = '127.0.0.1'
         port = '8080'
-        path = './public/index.php'
+        public = './public'
+        router = './public/index.php'
         
-        print(f"Server is running at {host} in port {port} and serving at path {path}")
+        print(f"Server is running at {host} in port {port} with router path {router} and serving at path {public}")
         print("To shutdown development server press control + c and please ignore the error")
 
-        system(f'php -S {host}:{port} {path}')
+        system(f'php -S {host}:{port} -t {public} {router}')
     pass
