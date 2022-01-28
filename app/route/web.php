f518e5eae39908ed934b0754ec15c958b537dd05
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2022-01-29 05:33:10 +0700

    update web route for more testing purposes

--- app/route/web.php
+++ app/route/web.php
@@ -1,2 +1,4 @@
 <?php
 $route->get('/', [Welcome::class, 'index'])->middleware('test');
+$route->get('/test', [Welcome::class, 'index'])->name('test')->middleware('test');
+// $route->get('/test/{number}', [Welcome::class, 'index'])->name('test')->whereNumber('number')->middleware('test'); // TODO for future SEO friendly URI
