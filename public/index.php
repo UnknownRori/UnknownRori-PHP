5cdc32f4f1d93c3a896642c8ef4901333ac50ab4
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2022-01-30 05:05:25 +0700

    little bit  path change

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 use Core\Kernel;
 
+// This is used for local development server using UnknownRoriCLI
 require_once('server.php');
+
+// This is used for web server
+// require_once('../server.php');
 
 return Kernel::Start();
--- server.php
+++ server.php
@@ -1,6 +1,12 @@
 <?php
 $_ENV['ROOT_PROJECT'] = __DIR__;
 
+// This is used for local development server using UnknownRoriCLI
 require_once('./vendor/autoload.php');
 require_once('./app/bootstrap.php');
 require_once('./core/bootstrap.php');
+
+// This is used for web server
+// require_once('vendor/autoload.php');
+// require_once('app/bootstrap.php');
+// require_once('core/bootstrap.php');
