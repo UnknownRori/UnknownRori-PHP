545f39d6484d95fd58fa988ed3a3f69d9cda315c
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2022-01-29 15:09:26 +0700

    added comment

--- app/config/db.php
+++ app/config/db.php
@@ -1,2 +1,6 @@
 <?php
+
+/**
+ * This is for Database PDO setting
+ */
 return [PDO::ERRMODE_EXCEPTION];
--- app/config/middleware.php
+++ app/config/middleware.php
@@ -1,9 +1,23 @@
 <?php
+
+/**
+ * This is where to register named middleware or group
+ */
 return [
+    /**
+     * Register middleware custom group middleware middleware class
+     * Middleware::RunAll('namedMiddleware');
+     * example : Middleware::RunAll('runtime');
+     */
     'runtime' => [
         App\Http\Middleware\test::class, // This is still for testing purposes
     ],
 
+    /**
+     * Register middleware named can be used using route or middleware class
+     * route : $route->get('uri', [controller:class, 'method'])->middleware('namedMiddleware');
+     * Middleware::Run('namedMiddleware');
+     */
     'named' => [
         'test' => App\Http\Middleware\test::class, // This is still for testing purposes
     ]
