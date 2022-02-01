b7de803eb61e835e6dcc3a5a579cc0b490a4311f
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2022-02-01 09:19:40 +0700

    change object property name to make more sense

--- core/Support/filesystem/Storage.php
+++ core/Support/filesystem/Storage.php
@@ -7,7 +7,7 @@ use Core\Support\Collection;
 class Storage
 {
     protected static $uploadDir = './public/storage';
-    protected static $defaultOption = [
+    protected static $option = [
         'overwrite' => false,
         'max-size' => 500000,
         'type' => 'image',
@@ -15,11 +15,10 @@ class Storage
 
     public static function upload($file, array $option = null)
     {
-        // dd($file);
-        self::$defaultOption = new Collection(self::$defaultOption);
+        self::$option = new Collection(self::$option);
         if (!is_null($option)) {
-            self::$defaultOption->fill($option);
-            self::$defaultOption->save();
+            self::$option->fill($option);
+            self::$option->save();
         };
         $target_upload = self::$uploadDir . '/' . basename($file["name"]);
         $file['type'] = explode('/', $file['type']);
@@ -27,10 +26,10 @@ class Storage
             $file['img_size'] = getimagesize($file["tmp_name"]);
         }
 
-        if ($file['type'][0] == self::$defaultOption->get('type')) {
-            if ($file['size'] < self::$defaultOption->get('max-size')) {
+        if ($file['type'][0] == self::$option->get('type')) {
+            if ($file['size'] < self::$option->get('max-size')) {
 
-                if (self::$defaultOption->get('overwrite') == true) {
+                if (self::$option->get('overwrite') == true) {
                     move_uploaded_file($file["tmp_name"], $target_upload);
                     return $target_upload;
                 } else {
