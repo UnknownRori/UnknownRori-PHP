5f4a59a97d7b42b080d104ec5b2569c4af9c982d
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2022-02-01 19:17:48 +0700

    added simple hasMany and belongsTo method

--- core/model/Model.php
+++ core/model/Model.php
@@ -7,6 +7,8 @@ use Core\Database\DB;
 class Model implements IModel
 {
     protected $table;
+    protected $hasMany;
+    protected $belongsTo;
 
     /**
      * Find specific id value inside the model table
@@ -23,7 +25,20 @@ class Model implements IModel
     public static function all()
     {
         $self = new static;
-        return DB::table($self->table)->all();
+        if ($self->belongsTo()) {
+            $data = $self->belongsTo();
+            $foreign_model = new $data[0]();
+            $query = "SELECT * FROM {$self->table} LEFT JOIN {$foreign_model->table} ON {$self->table}.{$data[1]} = {$foreign_model->table}.id";
+            return DB::prepare($query)->fetchAll();
+        } else if ($self->hasMany()) {
+            $data = $self->hasMany();
+            $foreign_model = new $data[0]();
+            $query = "SELECT * FROM {$self->table} LEFT OUTER JOIN {$foreign_model->table} ON {$self->table}.id = {$foreign_model->table}.{$data[1]}";
+            // dd($query);
+            return DB::prepare($query)->fetchAll();
+        } else {
+            return DB::table($self->table)->all();
+        }
     }
 
     /**
@@ -42,5 +57,15 @@ class Model implements IModel
     {
         $self = new static;
         return DB::table($self->table)->paginate($perPage);
+    }
+
+    protected function belongsTo()
+    {
+        return $this->belongsTo;
+    }
+
+    protected function hasMany()
+    {
+        return $this->hasMany;
     }
 }
