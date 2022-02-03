c3c4227f4ac6cabe9c3d8c81cbb7ad9a62500b84
i74ifa <i74ifa@gmail.com>
2022-02-03 11:56:24 +0300

    Add symfony dump to add dd and dump function

--- core/utils/debug.php
+++ core/utils/debug.php
@@ -3,33 +3,33 @@
 /**
  * Your typical Die and Dump in Laravel, but different
  */
-function dd(...$arguments)
-{
-    echo "<style>
-        pre.dump {
-            background-color: #18171B;
-            color: white;
-            line-height: 1.2em;
-            font: 12px Menlo, Monaco, Consolas, monospace;
-            word-wrap: break-word;
-            white-space: pre-wrap;
-            position: relative;
-            z-index: 99999;
-            word-break: break-all;
-        }
-        pre.dump:after {
-            content: '';
-            visibility: hidden;
-            display: block;
-            height: 0;
-            clear: both;
-        }
-    </style>";
-    echo '<pre class="dump">';
-    var_dump(...$arguments);
-    echo '</pre>';
-    die;
-}
+// function dd(...$arguments)
+// {
+//     echo "<style>
+//         pre.dump {
+//             background-color: #18171B;
+//             color: white;
+//             line-height: 1.2em;
+//             font: 12px Menlo, Monaco, Consolas, monospace;
+//             word-wrap: break-word;
+//             white-space: pre-wrap;
+//             position: relative;
+//             z-index: 99999;
+//             word-break: break-all;
+//         }
+//         pre.dump:after {
+//             content: '';
+//             visibility: hidden;
+//             display: block;
+//             height: 0;
+//             clear: both;
+//         }
+//     </style>";
+//     echo '<pre class="dump">';
+//     var_dump(...$arguments);
+//     echo '</pre>';
+//     die;
+// }
 
 /**
  * Gets the value of an environment variable.
