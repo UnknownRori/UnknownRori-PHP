ce2b20ce2ebc43a6ccc2145b7dd7b8a3dc7ca163
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2022-02-04 03:39:41 +0700

    refactor session get method

--- core/Support/session/ISession.php
+++ core/Support/session/ISession.php
@@ -4,7 +4,7 @@ namespace Core\Support;
 
 interface ISession
 {
-    public static function get($name);
+    public static function get($key = null);
     public static function set($name, $value);
     public static function destroy();
     public static function unset($name);
--- core/Support/session/Session.php
+++ core/Support/session/Session.php
@@ -4,7 +4,10 @@ namespace Core\Support;
 
 class Session implements ISession
 {
-
+    /**
+     * Initialize Session data and should be used in Kernel.php,
+     * this method can be passed with an array of option which can be modified in `app/config/kernel.php`
+     */
     public static function start($option = [])
     {
         return session_start($option);
@@ -13,9 +16,11 @@ class Session implements ISession
     /**
      * Get Session Data
      */
-    public static function get($name)
+    public static function get($key = null)
     {
-        return $_SESSION[$name];
+        if (!is_null($key)) return $_SESSION;
+
+        return $_SESSION;
     }
 
     /**
