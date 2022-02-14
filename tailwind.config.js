b2d0dbcbecfdb26f3baed15d091ea4fc42ade3f5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-02-15 03:18:49 +0700

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./app/views/*.{php,blade.php}",
+        "./resource/js/*.js",
+        "./public/js/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
