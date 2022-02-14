dab741be8759109464b85c71535fddc7d975987a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-02-14 16:56:28 +0700

    add : Str class

---
+++ core/Support/Str/Str.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Core\Support\Str;
+
+class Str
+{
+    protected $data;
+
+    /**
+     * initialize UnknownRori\Str\Str Instance
+     * @param  string $string
+     * @return $this
+     */
+    public function __construct($string)
+    {
+        $this->data = $string;
+        return $this;
+    }
+
+    /**
+     * Uppercase string
+     * @return string
+     */
+    public function upper()
+    {
+        return $this->data = strtoupper($this->data);
+    }
+
+    /**
+     * Split the string into an array
+     * @param  int   $length
+     * @return array
+     */
+    public function split($length = 100)
+    {
+        return $this->data = str_split($this->data, $length);
+    }
+
+    /**
+     * Explode the string into array
+     * @param  string  $seperator
+     * @return array
+     */
+    public function explode($seperator)
+    {
+        return $this->data = explode($seperator, $this->data);
+    }
+
+    /**
+     * Trim the string
+     * @param  string $string
+     * @return $this
+     */
+    public function ltrim($string)
+    {
+        return $this->data = ltrim($this->data, $string);
+    }
+
+    /**
+     * Uppercase the every first word
+     * @return string
+     */
+    public function upperfirst()
+    {
+        return $this->data = ucfirst($this->data);
+    }
+
+    /**
+     * Uppercase the first word
+     * @return string
+     */
+    public function upperfirstword()
+    {
+        return $this->data = ucwords($this->data);
+    }
+
+    /**
+     * Count word inside a string
+     * @return int
+     */
+    public function count()
+    {
+        return str_word_count($this->data);
+    }
+
+    /**
+     * Count the length of the string
+     * @return int
+     */
+    public function length()
+    {
+        return strlen($this->data);
+    }
+
+    /**
+     * Lowercase the string
+     * @return string
+     */
+    public function lower()
+    {
+        return $this->data = strtolower($this->data);
+    }
+
+    /**
+     * Get the string
+     * @return string
+     */
+    public function get()
+    {
+        return $this->data;
+    }
+}
