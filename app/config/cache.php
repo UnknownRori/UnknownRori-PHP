6f16342bb5d77bff28549472854047606c7bf5b4
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-03-13 07:36:26 +0700

    initial commit Cache class, implemented soon

---
+++ app/config/cache.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'type' => 'filesystem',
+    'default_timeout' => 3600,
+    'cache_type' => 'json',
+];
---
+++ core/Support/cache/Cache.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Core\Support\Cache;
+
+class Cache implements ICache
+{
+    //
+}
