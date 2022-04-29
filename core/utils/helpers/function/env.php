6f59d131ff9d3ecf397cdc0cade49385af68a538
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-04-29 16:16:34 +0700

    implement automatically convert boolean, empty, null

--- core/utils/helpers/function/env.php
+++ core/utils/helpers/function/env.php
@@ -10,6 +10,25 @@
 function env($key, $default = null)
 {
     if (isset($_ENV[$key])) {
+
+        switch (strtolower($_ENV[$key])) {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+
+            case 'null':
+            case '(null)':
+                return;
+        }
+
         return $_ENV[$key];
     }
 
