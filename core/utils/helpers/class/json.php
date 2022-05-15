cb4dda42e9eae893ec862f0d7377208bdb198d99
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-15 07:09:53 +0700

    add more parameter

--- core/utils/helpers/class/json.php
+++ core/utils/helpers/class/json.php
@@ -8,20 +8,25 @@ class Json
     /**
      * Encode the passed argumments to json
      * @param  mixed $data
+     * @param  int   $flag
+     * @param  int   $depth
      * @return string
      */
-    public static function Encode(mixed $data)
+    public static function Encode(mixed $data, int $flag = 0, int $depth = 512)
     {
-        return json_encode($data);
+        return json_encode($data, $flag, $depth);
     }
 
     /**
      * Decode the passed json argumments
      * @param  mixed $data
+     * @param  bool  $associative
+     * @param  int   $flag
+     * @param  int   $depth
      * @return mixed
      */
-    public static function Decode(mixed $data)
+    public static function Decode(mixed $data, bool $associative = true, int $depth = 512, int $flag = 0)
     {
-        return json_decode($data);
+        return json_decode($data, $associative, $depth, $flag);
     }
 }
