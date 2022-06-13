ee675cae59e063b99da23b564f4aba22a771162b
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-06-13 20:06:17 +0700

    add table property as the model generated

--- core/console/template/model.php
+++ core/console/template/model.php
@@ -5,11 +5,12 @@ function controller($name)
     return "<?php
 
 namespace App\Models;
+    
 use Core\BaseModel;
 
 class {$name} extends BaseModel
 {
-    // Code Here
+    protected \$table = '{$name}';
 }
 
 ";
