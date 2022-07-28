90f534b5306aaed768e465b8158dfd57a9f5cb19
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-29 05:53:03 +0700

    📦 Bumb the version

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
 $_ENV['ROOT_PROJECT'] = __DIR__;
-$_ENV['APP_VERSION'] = '2.0.3';
+$_ENV['APP_VERSION'] = '2.1.0';
 
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/app/bootstrap.php');
